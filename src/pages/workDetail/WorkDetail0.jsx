222d623404ede2aaeff030075195439e80bce52d
choidongwon <ddungpal@naver.com>
2023-08-01 23:31:00 +0900

    add workpage 0_2

--- src/pages/workDetail/WorkDetail0.jsx
+++ src/pages/workDetail/WorkDetail0.jsx
@@ -36,21 +36,8 @@ export default function WorkDetail0() {
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <div className="container mx-auto px-5 lg:px-1">
-        <div className="">
-          <p className="mb-3 text-xl font-bold">
-            Choi Ji Won
-            <br />
-            Yellow Forest
-          </p>
-          <p className="line-clamp text-justify">
-            <span className="font-bold mr-2">Choi Ji Won</span>
-            <span>
-              is a visual artist who works in drawing, painting, and
-              multidisciplinary arts. Her work may seem simple and empty, but
-              there’s an energy and rhythm about to flow somewhere. Her abstract
-              art quietly seeps in your mind and lingers.
-            </span>
-          </p>
+        <div className="mt-5">
+          <p className="w-3/5 mx-auto mb-3 text-xl font-bold">Yellow Forest</p>
           <div className="w-3/5 mx-auto">
             <img
               className="project__img mb-2"
@@ -60,18 +47,6 @@ export default function WorkDetail0() {
           </div>
           <p className="text-gray-400 text-center mb-5">
             “Yellow Forest” still cut
-          </p>
-          <p className="line-clamp text-justify">
-            <span className="font-bold mr-2">“Yellow Forest”</span>
-            <span>
-              is a piece inspired by the forest. As grass sways in the wind, it
-              conjures the moment of becoming one with nature, with indistinct
-              lumps representing one with nature, with indistinct lumps
-              representing people seen among the grass. It signifies the
-              artist’s experience in the still yet dynamic forest, told through
-              brush strokes as the sensations stay concentrated in the tips of
-              her fingers.
-            </span>
           </p>
           <video
             className="mb-5 mx-auto"
@@ -83,6 +58,29 @@ export default function WorkDetail0() {
           <p className="text-gray-400 text-center mb-10">
             “Yellow Forest” teaser
           </p>
+          <div className="w-3/5 mx-auto mb-10">
+            <p className="line-clamp text-justify">
+              <span className="font-bold mr-2">Choi Ji Won</span>
+              <span>
+                is a visual artist who works in drawing, painting, and
+                multidisciplinary arts. Her work may seem simple and empty, but
+                there’s an energy and rhythm about to flow somewhere. Her
+                abstract art quietly seeps in your mind and lingers.
+              </span>
+            </p>
+            <p className="line-clamp text-justify">
+              <span className="font-bold mr-2">“Yellow Forest”</span>
+              <span>
+                is a piece inspired by the forest. As grass sways in the wind,
+                it conjures the moment of becoming one with nature, with
+                indistinct lumps representing one with nature, with indistinct
+                lumps representing people seen among the grass. It signifies the
+                artist’s experience in the still yet dynamic forest, told
+                through brush strokes as the sensations stay concentrated in the
+                tips of her fingers.
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </>
